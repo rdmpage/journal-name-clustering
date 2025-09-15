0387ea16a7a4be010194868e8b1bb46c39e09e7f
Roderic Page <rdmpage@gmail.com>
2025-09-15 13:37:23 +0100

    Add clustering

--- README.md
+++ README.md
@@ -16,3 +16,27 @@ php import.php data/serials_2025-06-16T20_16_58+00_00.tsv > clusters.csv
 
 Create a SQLite database `containers` and import the cleaned journal names into a table called `clusters`.
 
+### Bad names
+
+The Taxonworks data is messy, journal names such as “p” will cause spurious clusters as everything with the sortkey “p” is potentially part of that cluster. Hence we remove those from the data.
+
+### Clustering
+
+The journal names are cleaned of extraneous characters, split into tokens, and the first letter of each token is extracted. These letters are concatenated to create a “sortkey” for each name.
+
+As a first pass we cluster using the sortkey to “block” the data, that is, we use the sortkey to subset the data into manageable chuncks. Journal names with the same sortkey are then  clustered based on the similarity of their cleaned, tokenised names. We use a [disjoint set](https://en.wikipedia.org/wiki/Disjoint-set_data_structure) to hold the clusters.
+
+To cluster the journals run:
+
+```
+php cluster.php > <output filename>
+```
+
+which output the SQL commands to update the `clusters` table.
+
+### Output
+
+The query `SELECT * FROM clusters ORDER BY sortkey, cluster_id;` generates a list of clustered journal names. This list has been used to create a Google Sheet, with an additional column that, together with conditional formatting, makes any additional names in a cluster a different colour, making the clustering more visible.
+
+The Google Sheet is https://docs.google.com/spreadsheets/d/1130fgDii0WodZIookBOg-KICZAdlk6lBVb1iTENPcr4/edit?usp=sharing
+
---
+++ cluster.php
@@ -0,0 +1,284 @@
+<?php
+
+// For each sortkey in clusters table load all names and compare strings based on 
+// a chosen measure (e.g., longest common subsequence). 
+// Use disjoint set datastructure to find connected components of graph where nodes 
+// are names and edges link names that are deemed sufficiently similar to be part of
+// the same cluster.
+
+require_once (dirname(__FILE__) . '/clean.php');
+require_once (dirname(__FILE__) . '/disjoint_set.php');
+require_once (dirname(__FILE__) . '/sqlite.php');
+
+//----------------------------------------------------------------------------------------
+// Get unique cleaned strings and use them to create a map between cleaned strings
+// and local ids
+function unique_cleaned_strings($input)
+{
+	$strings = [];
+	
+	foreach ($input as $string => $ids)
+	{
+		$string = normalise_text($string);
+		$string = removeCommonWords($string);
+		
+		//echo $string . "\n";
+		
+		if (!isset($strings[$string]))
+		{
+			$strings[$string] = [];
+		}
+		
+		// print_r($ids);
+		
+		foreach ($ids as $id)
+		{
+			$strings[$string][] = $id;
+		}
+	
+	}
+	
+	return $strings;
+}
+
+//----------------------------------------------------------------------------------------
+function compare_strings($string1, $string2, $parameters, $debug = false)
+{
+   	$tokens1 = tokenise_string($string1);
+   	$tokens2 = tokenise_string($string2);
+	
+	$result = longestCommonSubsequence($tokens1, $tokens2, $parameters);
+	
+	if ($debug)
+	{
+		print_r($result);
+	}
+	
+	return $result['d'];
+}
+
+//----------------------------------------------------------------------------------------
+// token Longest common subsequence
+function longestCommonSubsequence(array $tokens1, array $tokens2, $parameters)
+{
+	//$min_match_length 	= 2;
+	//$extra_match_score 	= 0.1;
+	//$max_character_diff = 1;
+
+    $m = count($tokens1);
+    $n = count($tokens2);
+
+    // Initialize the DP table
+    $dp = array_fill(0, $m + 1, array_fill(0, $n + 1, 0));
+    $trace = array_fill(0, $m + 1, array_fill(0, $n + 1, ''));
+
+    // Fill the DP table and trace info
+    for ($i = 1; $i <= $m; $i++) {
+        for ($j = 1; $j <= $n; $j++) {
+        
+        	$sim = 0;
+        	
+        	// is one string a prefix of the other?
+        	if (is_abbreviation($tokens1[$i - 1], $tokens2[$j - 1]))
+        	{
+        		$sim = 1;
+        		
+        		// are they non-trivial matches? 
+                if (($tokens1[$i - 1] === $tokens2[$j - 1]) && strlen($tokens1[$i - 1]) >= $parameters['min_match_length'])
+                {
+                	$sim += $parameters['extra_match_score'];
+                }        		
+        	}
+        	else
+        	{
+        		// if not substrings, are the two strings similar, say differing by a single letter?
+        		if (levenshtein($tokens1[$i - 1], $tokens2[$j - 1]) <= $parameters['max_character_diff'])
+        		{
+        			$sim = 1;
+        		}        	
+        	}
+        	
+            if ($sim > 0)
+            {
+                $dp[$i][$j] = $dp[$i - 1][$j - 1] + $sim;
+                 
+                $trace[$i][$j] = 'diag';  // match
+            } elseif ($dp[$i - 1][$j] >= $dp[$i][$j - 1]) {
+                $dp[$i][$j] = $dp[$i - 1][$j];
+                $trace[$i][$j] = 'up';    // move up
+            } else {
+                $dp[$i][$j] = $dp[$i][$j - 1];
+                $trace[$i][$j] = 'left';  // move left
+            }
+        }
+    }
+
+    // Reconstruct the LCS sequence via traceback
+    $lcs = [];
+    $i = $m;
+    $j = $n;
+    while ($i > 0 && $j > 0) {
+        if ($trace[$i][$j] === 'diag') {
+            $lcs[] = [$tokens1[$i - 1],$tokens2[$j - 1]];
+            $i--;
+            $j--;
+        } elseif ($trace[$i][$j] === 'up') {
+            $i--;
+        } else {
+            $j--;
+        }
+    }
+    
+    // score for longest subsequence
+    $score = $dp[$m][$n];
+   
+    // normalised score
+    $d = 2 * $dp[$m][$n] / ($m + $n + 2 * min($m,$n) * $parameters['extra_match_score']);
+    $d = round($d, 2);
+    
+    return [
+        'score' => $score,
+        't1' => join(' ', $tokens1),
+        't2' => join(' ', $tokens2),
+        'd' => $d,
+        array_reverse($lcs)
+    ];
+
+}
+
+//----------------------------------------------------------------------------------------
+	
+function cluster_sortkeys($sortkeys, $parameters, $debug = false)
+{
+	$sql = "SELECT * FROM clusters WHERE sortkey IN ('" . join("','", $sortkeys) . "')";
+	
+	$data  = db_get($sql);
+	
+	$values = array();
+	
+	$input = array();
+	
+	foreach ($data as $row)
+	{
+		if (!isset($input[$row->cleaned]))
+		{
+			$input[$row->cleaned] = array();
+		}
+		$input[$row->cleaned][] = $row->id;
+	}
+	
+	if ($debug)
+	{
+		print_r($input);
+	}
+	
+	$clean_string_map = unique_cleaned_strings($input);
+	
+	// print_r($clean_string_map);
+	
+	// compare cleaned strings
+	$strings = array_keys($clean_string_map);
+	
+	if ($debug)
+	{
+		print_r($strings);
+	}
+	
+	$dj = new DisjointSet();
+	foreach ($clean_string_map as $string => $ids)
+	{
+		foreach ($ids as $id)
+		{
+			$dj->makeset($id);
+		}
+		
+		if (count($ids) > 1)
+		{
+			$n = count($ids);
+			for ($i = 1; $i < $n; $i++)
+			{
+				$dj->union($ids[$i], $ids[0]);
+			}
+		}
+	}
+	
+	$n = count($strings);
+	
+	for ($i = 0; $i < $n - 1; $i++)
+	{
+		for ($j = $i + 1; $j < $n; $j++)
+		{
+			$d = compare_strings($strings[$i], $strings[$j], $parameters, $debug);
+			
+			// if reasonably similar add to set
+			if ($d >= $parameters['threshold'])
+			{
+				$dj->union($clean_string_map[$strings[$i]][0], $clean_string_map[$strings[$j]][0]);
+			}
+		}
+	}
+	
+	if ($debug)
+	{
+		echo $dj->dot();
+	}
+	
+	$clusters = $dj->clusters();
+	
+	foreach ($clusters as $index => $members)
+	{
+		foreach ($members as $i)
+		{
+			echo "UPDATE clusters SET cluster_id=$index WHERE id=$i;\n";
+		}
+	}
+
+}
+
+
+$debug = true;
+$debug = false;
+
+
+$parameters = array(
+	'threshold' 		 => 0.9,
+	'min_match_length' 	 => 2,
+	'extra_match_score'  => 0.1,
+	'max_character_diff' => 1
+);
+
+// debugging so we try some examples
+if (0)
+{
+	$sortkeys = array('em');
+	//$sortkeys = array('jbnhs');
+	//$sortkeys = array('trssa');
+	
+	$sortkeys = array('sez');
+	
+	$sortkeys = array('rsz');
+	
+	$sortkeys = array('e');
+}
+else
+{
+	// Build clusters for everything	
+	$sql = "SELECT DISTINCT sortkey FROM clusters WHERE sortkey IS NOT NULL AND sortkey != ''";
+	
+	$sortkeys = array();
+	
+	$data  = db_get($sql);
+	
+	foreach ($data as $row)
+	{
+		$sortkeys [] = $row->sortkey;
+	}
+}
+
+foreach ($sortkeys as $s)
+{
+	cluster_sortkeys([$s], $parameters, $debug);
+}
+
+?>
+
